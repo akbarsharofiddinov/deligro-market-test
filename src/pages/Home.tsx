d25ed70428e7c3e261235ca0fc0c7326b56ddadf
akbarsharofiddinov <akbarjonsharofiddino2004@gmail.com>
2024-08-21 12:43:29 +0500

    .

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,14 +7,14 @@ import {
   SearchInput,
 } from "@/components";
 import { useEdisonContext } from "@/context/EdisonContext";
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+// import { useEffect } from "react";
+// import { useNavigate } from "react-router-dom";
 
 function Home() {
   const { isOpen } = useEdisonContext();
 
-  const navigate = useNavigate();
-  let tg = Telegram.WebApp;
+  // const navigate = useNavigate();
+  // let tg = Telegram.WebApp;
 
   // useEffect(() => {
   //   if (tg.initDataUnsafe.user?.id === undefined) {
